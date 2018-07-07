22cf134044ec1596b1dccb654b3cf9099f76af07
jurek <jurek>
2018-07-07 19:57:14 +0200

    dodany imagemagick i paperclip, zmienione widoki

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,9 @@
 class Pin < ApplicationRecord
 	belongs_to :user
+	# dopisuje do obslugi obrazow przez ImageMagick i paperclip
+	# 300x300 do wymiar obrazu, 100x100 to jego miniatura
+	# do wymiarow bedzie sie mozna odwolywac przez :medium lub :thumb
+	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }
+	# tutaj za pomoca wyrazenia regularnego zdefiniowano typ pliku graficznego
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
